f720b496cd1e7e3580f44bee6f11be275cfad0d0
Daniel Moreira Pinto <48635337+DanielMoreiraPinto@users.noreply.github.com>
2020-02-25 17:48:24 -0300

    App's idea is made, added new screens, going for view navigation, may god protect me

--- App.js
+++ App.js
@@ -9,59 +9,19 @@ import {
   StatusBar,
   Button,
   ImageBackground,
+  SectionList,
 } from 'react-native';
 
 const App: () => React$Node = () => {
   return (
     <>
-      <View style={styles.container1}>
-        <Button
-          onPress={() => {
-            alert("Tela de arte");
-          }}
-          title="A arte"
-          style={styles.botao}
-        >
-        </Button>
-        <Button
-          onPress={() => {
-            alert("Tela de história");
-          }}
-          title="A história"
-          style={styles.botao}
-        >
-        </Button>
-        <Button
-          onPress={() => {
-            alert("Tela do profissional");
-          }}
-          title="O profissional"
-          style={styles.botao}
-        >
-        </Button>
-        <Button
-          onPress={() => {
-            alert("Tela das uriosidades");
-          }}
-          title="Curiosidades"
-          style={styles.botao}
-        >
-        </Button>
-      </View>
+      
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  botao: {
-    color: "#0a0f52"
-  },
-
-  container1: {
-    flex: 1,
-    justifyContent: "space-around",
-    alignItems: "center"
-  }
+  
 });
 
 export default App;
---
+++ TelaExploracao.js
@@ -0,0 +1,44 @@
+<View style={styles.container1}>
+    <Button
+        onPress={() => {
+        alert("Tela de arte");
+        }}
+        title="A arte"
+        style={styles.botao}
+    >
+    </Button>
+    <Button
+        onPress={() => {
+        alert("Tela de história");
+        }}
+        title="A história"
+        style={styles.botao}
+    >
+    </Button>
+    <Button
+        onPress={() => {
+        alert("Tela do profissional");
+        }}
+        title="O profissional"
+        style={styles.botao}
+    >
+    </Button>
+    <Button
+        onPress={() => {
+        alert("Tela das curiosidades");
+        }}
+        title="Curiosidades"
+        style={styles.botao}
+    >
+    </Button>
+</View>
+
+botao: {
+    color: "#0a0f52"
+  },
+
+  container1: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center"
+  }
